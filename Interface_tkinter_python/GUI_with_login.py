ada5cc53f2234c77b6cf66758696e17b9f8ff69c
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-04 14:27:23 +0100

    add the main button for login or registration in GUI

--- Interface_tkinter_python/GUI_with_login.py
+++ Interface_tkinter_python/GUI_with_login.py
@@ -5,3 +5,31 @@
 '''
 from tkinter import*
 
+# def the fauntion fro the main screen 
+
+def  main_acount_screen():
+
+	#1-Create the GUI window, 2- Set the parameters size then thetitleof the GUI window 
+	main_screen = Tk()
+	main_screen.geometry("600x500")
+	main_screen.title("Account Register&Login")
+
+	Label(text="Choose Login Or Register", bg="red", width="300", height="2", font=("Calibri", 15)).pack() 
+	Label(text="").pack()
+
+	#creat a quit button 
+	Button(main_screen, text='QUIT', command=main_screen.quit).pack(side=RIGHT)
+
+	Button(text="Login", height="2", width="30",font=("Calibri", 13)).pack() 
+	Label(text="").pack() 
+
+	Button(text="Register", height="2", width="30",font=("Calibri", 13)).pack()
+
+	#start the GUI 
+	main_screen.mainloop()
+
+
+#Try the GUI interface 
+
+if __name__=='__main__':
+	main_acount_screen()
