297321dcc810c1f4fea504f69c724cf722d648b2
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-04 18:26:54 +0100

    add login function

--- Interface_tkinter_python/GUI_with_login.py
+++ Interface_tkinter_python/GUI_with_login.py
@@ -113,7 +113,31 @@ def register_user():
 	Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
 
 '''Designing New Screen For Login'''
-
+# define login function
+def login():
+    
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+ 
+    global username_verify
+    global password_verify
+ 
+    username_verify = StringVar()
+    password_verify = StringVar()
+ 
+   
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password__login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password__login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command=login_verification).pack()
 
 #Try the GUI interface 
 
