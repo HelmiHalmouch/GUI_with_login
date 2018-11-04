7a9ea425f81798e24755a17f854605e8e36770ff
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-04 19:23:45 +0100

    full complete login versiifcation

--- Interface_tkinter_python/GUI_with_login.py
+++ Interface_tkinter_python/GUI_with_login.py
@@ -4,7 +4,7 @@
    Description: Creat a Graphical User Interface (GUI) with login in Tikinter python 
 '''
 from tkinter import*
-import suy, os 
+import sys, os 
 
 # def the fauntion fro the main screen 
 
@@ -119,7 +119,7 @@ def register_user():
 '''Designing New Screen For Login'''
 # define login function
 def login():
-    
+    global login_screen
     login_screen = Toplevel(main_screen)
     login_screen.title("Login")
     login_screen.geometry("300x250")
@@ -132,20 +132,22 @@ def login():
     username_verify = StringVar()
     password_verify = StringVar()
  
-   
+    global username_login_entry
+    global password_login_entry
+ 
     Label(login_screen, text="Username * ").pack()
     username_login_entry = Entry(login_screen, textvariable=username_verify)
     username_login_entry.pack()
     Label(login_screen, text="").pack()
     Label(login_screen, text="Password * ").pack()
-    password__login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
-    password__login_entry.pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password_login_entry.pack()
     Label(login_screen, text="").pack()
-    Button(login_screen, text="Login", width=10, height=1, command=login_verification).pack()
+    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
 
 ''' Define login verification function'''
-def login_verification():
-	print('working....')
+'''def login_verification():
+	print('working....')'''
 
 def login_verify():
 
@@ -181,13 +183,13 @@ def login_verify():
 
 def login_sucess():
 	global login_success_screen   # make login_success_screen global
-    login_success_screen = Toplevel(login_screen)
-    login_success_screen.title("Success")
-    login_success_screen.geometry("150x100")
-    Label(login_success_screen, text="Login Success").pack()
- 
+	login_success_screen = Toplevel(login_screen)
+	login_success_screen.title("Success")
+	login_success_screen.geometry("150x100")
+	Label(login_success_screen, text="Login Success").pack()
+
 	# create OK button
-    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+	Button(login_success_screen, text="OK", command=delete_login_success).pack()
 	
 def delete_login_success():
     login_success_screen.destroy()
