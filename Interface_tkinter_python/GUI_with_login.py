72dd6e69c41a746bd376a25bc26cecb47e45536d
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-04 14:06:21 +0100

    add main script of the GUI using tikinter

---
+++ Interface_tkinter_python/GUI_with_login.py
@@ -0,0 +1,7 @@
+'''Title :Graphical User Interface with Login
+   Authors: GHANMI Helmi
+   Date : 04 November 2018
+   Description: Creat a Graphical User Interface (GUI) with login in Tikinter python 
+'''
+from tkinter import*
+
